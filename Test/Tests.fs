80ec5e560dd66bd90065ae84d13ba68cb84082bd
uno <uno.stefano@gmail.com>
2018-08-26 19:31:29 +0100

    Add operations

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -4,8 +4,43 @@ open System
 open Xunit
 
 [<Fact>]
-let ``ls / shows root directory content`` () =
+let ``ls shows the content of a directory`` () =
     let directories = 
         Operations.ls "/"
+    
+    Assert.True(true)
+
+[<Fact>]
+let ``mkdir creates a directory`` () =
+    let directory = 
+        Operations.mkdir "/" "directory"
+    
+    Assert.True(true)
+    
+[<Fact>]
+let ``touch creates a file`` () =
+    let file = 
+        Operations.touch "/" "file"
+    
+    Assert.True(true)
+    
+[<Fact>]
+let ``cd enters a directory`` () =
+    let directory = 
+        Operations.cd "/" "directory"
+    
+    Assert.True(true)
+    
+[<Fact>]
+let ``read reads the content of a file`` () =
+    let content = 
+        Operations.read "/" "file"
+    
+    Assert.True(true)
+    
+[<Fact>]
+let ``write writes content to a file`` () =
+    let file = 
+        Operations.write "/" "file" "content"
     
     Assert.True(true)
--- UnoFS/Operations.fs
+++ UnoFS/Operations.fs
@@ -2,3 +2,18 @@ module Operations
 
 let ls path =
     ()
+    
+let mkdir path name =
+    ()
+    
+let touch path name =
+    ()
+    
+let cd path name =
+    ()
+    
+let read path name =
+    ()
+    
+let write path name content =
+    ()
