0c9e4b7e52aa14b802a4520ff5173daa303e4b91
uno <uno.stefano@gmail.com>
2018-08-27 20:36:18 +0100

    Added REPL history and completion

--- TestRepl/Program.fs
+++ TestRepl/Program.fs
@@ -2,8 +2,23 @@
 open Commands
 open Types
 
+let commands =
+    [| "ls"; "mkdir "; "cd " |]
+
+type Handler () =
+    interface IAutoCompleteHandler with 
+        member __.GetSuggestions(text, index) =
+            commands |>
+            Array.filter (fun command -> command.StartsWith(text))
+        member __.Separators
+            with get() = [||]
+            and set(c) = ()
+
+ReadLine.HistoryEnabled <- true
+Handler () |> ReadLine.set_AutoCompletionHandler
+
 let rec replLoop workingDir =
-    match Console.ReadLine() with
+    match ReadLine.Read("> ") with
     | "exit"  -> ()
     | command -> processCommand workingDir command |>
                  replLoop
