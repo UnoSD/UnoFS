d613d82ba75aeb95cf092e891e0604f38bb0f46c
uno <uno.stefano@gmail.com>
2018-08-27 19:50:16 +0100

    Refactoring DirectoryChange

--- UnoFS/DirectoryChange.fs
+++ UnoFS/DirectoryChange.fs
@@ -3,26 +3,42 @@ module DirectoryChange
 open Types
 open System
 
+let private getDirFromRoot root segment =
+    root.Tags |>
+    Map.tryFindKey (fun tag _ -> tag.Name = segment) |>
+    Option.map (fun tag -> Child { Root = root; Tag = tag; Hierarchy = [] })
+
+let private getParentDirFrom child =
+    match child.Hierarchy with
+    | []      -> Root child.Root
+    | [x]     -> Child { Root = child.Root; Tag = x; Hierarchy = [] }
+    | x :: xs -> Child { Root = child.Root; Tag = x; Hierarchy = xs }
+
+let private hierarchyHas tag child =
+    child.Hierarchy |> List.contains tag
+
+let private getDirectoryFor tag child =
+    {
+        Root = child.Root
+        Tag = tag
+        Hierarchy = child.Tag :: child.Hierarchy
+    } |>
+    Child |>
+    Some
+
+let private getDirFromChild child segment =
+    match child.Root.Tags |> Map.tryFindKey (fun tag _ -> tag.Name = segment) with
+    | None                                    -> None
+    | Some tag when child.Tag = tag           -> None
+    | Some tag when child |> hierarchyHas tag -> None
+    | Some tag                                -> child |> getDirectoryFor tag
+
 let getRelativePath dir segment =
     match (dir, segment) with
-    | (Root _, "..")     -> None
-    | (Root r, segment)  -> r.Tags |>
-                            Map.tryFindKey (fun tag _ -> tag.Name = segment) |>
-                            Option.map (fun tag -> Child { Root = r; Tag = tag; Hierarchy = [] })
-    | (Child c, "..")    -> match c.Hierarchy with
-                            | []      -> Root c.Root |> Some
-                            | [x]     -> Child { Root = c.Root; Tag = x; Hierarchy = [] } |> Some
-                            | x :: xs -> Child { Root = c.Root; Tag = x; Hierarchy = xs } |> Some
-    | (Child c, segment) -> match c.Root.Tags |> Map.tryFindKey (fun tag _ -> tag.Name = segment) with
-                            | None                                           ->
-                                None
-                            | Some tag when c.Tag = tag ->
-                                None
-                            | Some tag when c.Hierarchy |> List.contains tag ->
-                                None
-                            | Some tag                                       ->
-                                Child { Root = c.Root; Tag = tag; Hierarchy = c.Tag :: c.Hierarchy } |>
-                                Some
+    | (Root _     , ".."   ) -> None
+    | (Root root  , segment) -> segment |> getDirFromRoot root
+    | (Child child, ".."   ) -> getParentDirFrom child |> Some
+    | (Child child, segment) -> segment |> getDirFromChild child
 
 let private pathFolder current segment =
     match current with
